fd0b235fc2f1fbf6c31e4077c7e199423f137129
Jamie White <jamie@jgwhite.co.uk>
2019-07-30 11:22:46 +0200

    Initial commit

---
+++ wit.js
@@ -0,0 +1,49 @@
+const sample = require("lodash.sample");
+
+module.exports = () => {
+  return sample(QUOTES);
+};
+
+const QUOTES = [
+  "My chances of being PM are about as good as the chances of finding Elvis on Mars, or my being reincarnated as an olive.",
+  "I'd like thousands of schools as good as the one I went to, Eton.",
+  "What I worry about is that people are losing confidence, losing energy, losing enthusiams, and there's a real opportunity to get them into work.",
+  "My policy on cake is pro having it and pro eating it.",
+  "My speaking style was criticised by no less an authority than Arnold Schwarzenegger. It was a low moment, my friends, to have my rhetorical skills denounced by a monosyllabic Austrian cyborg.",
+  "I love tennis with a passion. I challenged Boris Becker to a match once and he said he was up for it but he never called back. I bet I could make him run around.",
+  "There is absolutely no one, apart from yourself, who can prevent you, in the middle of the night, from sneaking down to tidy up the edges of that hunk of cheese at the back of the fridge.",
+  "The Lib Dems are not just empty. They are a void within a vacuum surrounded by a vast inanition.",
+  "Some people play the piano, some do Sudoku, some watch television, some people go out to dinner parties. I write books.",
+  "I don't see why people are so snooty about Channel 5. It has some respectable documentaries about the Second World War. It also devotes considerable airtime to investigations into lap-dancing, and other related and vital subjects.",
+  "I think I was once given cocaine but I sneezed so it didn't go up my nose. In fact, it may have been icing sugar.",
+  "The dreadful truth is that when people come to see their MP they have run out of better ideas.",
+  "I don't believe that is necessarily any more dangerous than the many other risky things that people do with their free hands while driving - nose-picking, reading the paper, studying the A-Z, beating the children, and so on.",
+  "If we judged everybody by the stupid, unguarded things they blurt out to their nearest and dearest, then we wouldn't ever get anywhere.",
+  "I'm backing David Cameron's campaign out of pure, cynical self-interest.",
+  "I realise that there may be some confusion in my prescriptions between what I would do, what Maggie would do, and what the government is about to do or is indeed already doing... I don't think it much matters, because the three are likely to turn out to be one and the same.",
+  "I have not been more robust towards female rather than male assembly members and I do not believe I have been remotely sexist.",
+  "My friends, as I have discovered myself, there are no disasters, only opportunities. And, indeed, opportunities for fresh disasters.",
+  "But if people want to swim in the Thames, if they want to take their lives into their own hands, then they should be able to do so with all the freedom and exhilaration of our woad-painted ancestors.",
+  "London is a fantastic creator of jobs - but many of these jobs are going to people who don't originate in this country.",
+  "I forgot that to rely on a train, in Blair's Britain, is to engage in a crapshoot with the devil.",
+  "Ping-pong was invented on the dining tables of England in the 19th century, and it was called Wiff-waff! And there, I think, you have the difference between us and the rest of the world. Other nations, the French, looked at a dining table and saw an opportunity to have dinner; we looked at it an saw an opportunity to play Wiff-waff.",
+  "Times have been tough, the economy has been tough. But I want to bring forward a fantastic manifesto for taking the city forwards.",
+  "It is easy to make promises - it is hard work to keep them.",
+  "It is possible to have a pretty good life and career being a leech and a parasite in the media world, gadding about from TV studio to TV studio, writing inconsequential pieces and having a good time.",
+  "Never in my life did I think I would be congratulated by Mick Jagger for achieving anything.",
+  "I think the risks that people see of terrorism are incredibly important but we are very confident we have got the right people on it and the risks have been minimised.",
+  "I'm a one-nation Tory.",
+  "Chinese cultural influence is virtually nil, and unlikely to increase...",
+  "Too full of drugs, obesity, underachievement and Labour MPs.",
+  "He's a rather engaging geezer",
+  "I remember the guts streaming, and the stag turds spilling out on to the grass from within the ventral cavity ... this hunting is best for the deer",
+  "It was jolly nice. But apparently it is very different these days. Much stronger. I've become very illiberal about it. I don't want my kids to take drugs",
+  "The Americans were perfectly happy to go ahead and whack Saddam merely on the grounds that he was a bad guy, and that Iraq and the world would be better off without him; and so indeed was I.",
+  "No one obeys the speed limit except a motorised rickshaw.",
+  "What has the BBC come to? Toilets, that's what",
+  "Exams work because they're scary",
+  "A horse is a safer bet than the trains",
+  "Face it: it's all your own fat fault",
+  "Nothing excites compassion, in friend and foe alike, as much as the sight of you ker-splonked on the Tarmac with your propeller buried six feet under.",
+  "It is just flipping unbelievable. He is a mixture of Harry Houdini and a greased piglet. He is barely human in his elusiveness. Nailing Blair is like trying to pin jelly to a wall."
+];
